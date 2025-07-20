cf11b8aab0b35b6a8e6829820f3a584242953897
Lucas-Alexandre131 <lucasalexandredeoliveira329@gmail.com>
2025-07-20 20:52:10 -0300

    created a new route --> about and start to use --> link

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,8 +1,13 @@
+import Link from "next/link";
+
 export default function Home() {
   return (
     <main>
       <h1>Olá, Next.js!</h1>
       <p>Meu primeiro projeto funcionando 🎉</p>
+      <p>
+        <Link href="/about">Ir para About</Link>
+      </p>
     </main>
   );
 }
